e79f3c04ed7053a5cb70cfa4f22f12524f31649c
kkoma2623 <jiwook.dev@gmail.com>
2025-09-04 00:21:11 +0900

    add created_at and updated_at

--- src/main/java/me/connan/springbootdeveloper/domain/Article.java
+++ src/main/java/me/connan/springbootdeveloper/domain/Article.java
@@ -1,7 +1,14 @@
 package me.connan.springbootdeveloper.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@EntityListeners(AuditingEntityListener.class) // 엔티티의 생성 및 수정 시간을 자동으로 감시하고 기록
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,6 +33,14 @@ public class Article {
 
 	@Column(name = "content", nullable = false)
 	private String content;
+
+	@CreatedDate // 엔티티가 생성될 때 생성 시간 저장
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate // 엔티티가 수정될 때 수정 시간 저장
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
 
 	@Builder
 	public Article(String title, String content) {
