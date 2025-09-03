a01ca2bfa1959aaaef7286f26e7d38fcd5d8f060
kkoma2623 <jiwook.dev@gmail.com>
2025-09-03 23:38:54 +0900

    add CrossOrigin annotation

--- src/main/java/me/connan/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/connan/springbootdeveloper/controller/BlogApiController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import me.connan.springbootdeveloper.dto.ArticleResponse;
 import me.connan.springbootdeveloper.dto.UpdateArticleRequest;
 import me.connan.springbootdeveloper.service.BlogService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RestController
 public class BlogApiController {
