17f8b66d39c23e28276ca38c23c63a8392032c73
kkoma2623 <jiwook.dev@gmail.com>
2025-09-03 21:23:54 +0900

    Articles find by id GET test code

--- src/main/java/me/connan/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/connan/springbootdeveloper/controller/BlogApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,13 @@ public class BlogApiController {
 
 		return ResponseEntity.ok()
 			.body(articles);
+	}
+
+	@GetMapping("/api/articles/{id}")
+	public ResponseEntity<ArticleResponse> findArticle(@PathVariable("id") Long id) {
+		Article article = blogService.findById(id);
+
+		return ResponseEntity.ok()
+			.body(new ArticleResponse(article));
 	}
 }
