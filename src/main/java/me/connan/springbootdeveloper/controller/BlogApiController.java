074b1b040ed04ef200e37cf1cfe50a2627d7daa5
kkoma2623 <jiwook.dev@gmail.com>
2025-09-03 21:58:35 +0900

    add updateArticle in BlogApiController to pass test code and pass test

--- src/main/java/me/connan/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/connan/springbootdeveloper/controller/BlogApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import me.connan.springbootdeveloper.domain.Article;
 import me.connan.springbootdeveloper.dto.AddArticleRequest;
 import me.connan.springbootdeveloper.dto.ArticleResponse;
+import me.connan.springbootdeveloper.dto.UpdateArticleRequest;
 import me.connan.springbootdeveloper.service.BlogService;
 
 @RequiredArgsConstructor
@@ -57,5 +59,13 @@ public class BlogApiController {
 
 		return ResponseEntity.ok()
 			.build();
+	}
+
+	@PutMapping("/api/articles/{id}")
+	public ResponseEntity<Article> updateArticle(@PathVariable("id") Long id, @RequestBody UpdateArticleRequest request) {
+		Article updatedArticle = blogService.update(id, request);
+
+		return ResponseEntity.ok()
+			.body(updatedArticle);
 	}
 }
