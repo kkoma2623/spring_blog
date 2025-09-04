1dc1ac028e5b4ac9d25dd92d29f795d9dfea4b68
kkoma2623 <jiwook.dev@gmail.com>
2025-09-04 20:11:13 +0900

    update Article bug fix, separate POST and PUT Article

--- src/main/java/me/connan/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/connan/springbootdeveloper/controller/BlogViewController.java
@@ -7,12 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import me.connan.springbootdeveloper.domain.Article;
+import me.connan.springbootdeveloper.dto.AddArticleRequest;
 import me.connan.springbootdeveloper.dto.ArticleViewResponse;
+import me.connan.springbootdeveloper.dto.UpdateArticleRequest;
 import me.connan.springbootdeveloper.service.BlogService;
 
 @RequiredArgsConstructor
@@ -41,14 +44,17 @@ public class BlogViewController {
 			.body(articleViewResponse);
 	}
 
-	@PostMapping("/new-article")
-	public ResponseEntity<ArticleViewResponse> newArticle(@RequestParam(required = false) Long id) {
-		if (id == null) {
-			return ResponseEntity.status(HttpStatus.CREATED)
-				.body(new ArticleViewResponse());
-		}
-		Article article = blogService.findById(id);
-		return ResponseEntity.status(HttpStatus.ACCEPTED)
-			.body(new ArticleViewResponse(article));
+	@PostMapping("/articles")
+	public ResponseEntity<ArticleViewResponse> createArticle(@RequestBody AddArticleRequest request) {
+		Article savedArticle = blogService.save(request);
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(new ArticleViewResponse(savedArticle));
+	}
+
+	@PutMapping("/articles/{id}")
+	public ResponseEntity<ArticleViewResponse> updateArticle(@PathVariable Long id,
+		@RequestBody UpdateArticleRequest request) {
+		Article updatedArticle = blogService.update(id, request);
+		return ResponseEntity.ok(new ArticleViewResponse(updatedArticle));
 	}
 }
