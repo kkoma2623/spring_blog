8fb867a51db784cb2c3321a62660bed0b2239e10
kkoma2623 <jiwook.dev@gmail.com>
2025-09-04 16:25:46 +0900

    add user service method

---
+++ src/main/java/me/connan/springbootdeveloper/service/UserService.java
@@ -0,0 +1,25 @@
+package me.connan.springbootdeveloper.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import me.connan.springbootdeveloper.domain.User;
+import me.connan.springbootdeveloper.dto.AddUserRequest;
+import me.connan.springbootdeveloper.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public Long save(AddUserRequest addUserRequest) {
+		return userRepository.save(User.builder()
+				.email(addUserRequest.getEmail())
+				.password(bCryptPasswordEncoder.encode(addUserRequest.getPassword()))
+				.build())
+			.getId();
+	}
+}
